a8aea91099d1071f9e79e45963bce6d891d74dda
Melwyn <melwynt@gmail.com>
2016-06-26 22:20:28 +0100

    added routing rules

--- client/routes.js
+++ client/routes.js
@@ -1,6 +1,6 @@
 //public routes
 var exposed = FlowRouter.group({
-
+  // add code so the user is redirected to the home page if he tries to go to the login page while he is already logged in
 });
 
 exposed.route('/', {
@@ -10,4 +10,60 @@ exposed.route('/', {
           content: 'main'
         });
     }
+});
+
+exposed.route('/login', {
+    name: 'login',
+    action: function () {
+        BlazeLayout.render('loginLayout');
+    }
+});
+
+/*Private routes*/
+var loggedIn = FlowRouter.group({
+    triggersEnter: [
+        function () {
+            var route;
+            // if user is not logged in
+            if (!(Meteor.loggingIn() || Meteor.userId())) {
+                route = FlowRouter.current();
+                
+                // save the path
+                if (route.route.name !== 'login') {
+                    Session.set('redirectAfterLogin', route.path);
+                }
+                FlowRouter.go(FlowRouter.path('/login'));
+            }
+        }
+    ]
+});
+
+loggedIn.route('/admin', {
+    name: 'main',
+    action: function () {
+        BlazeLayout.render('mainLayout', {
+          content: 'admin',
+        });
+    }
+});
+
+// hook
+Accounts.onLogin(function () {
+  Meteor.logoutOtherClients();
+
+  Session.set ('loggedIn', true);
+
+  var redirect = Session.get('redirectAfterLogin');
+  
+  if (redirect != null) {
+    if (redirect !== 'login') {
+      FlowRouter.go(redirect);
+    }
+  }
+});
+
+Accounts.onLogout(function() {
+  console.log("I just logged out.");
+
+  FlowRouter.go(FlowRouter.path('/'));
 });
